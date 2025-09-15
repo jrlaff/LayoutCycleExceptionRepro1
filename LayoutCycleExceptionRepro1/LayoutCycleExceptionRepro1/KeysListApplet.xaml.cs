b9858cd116f548cc60c6d7868fb938c882ab6910
jrlaff <72731879+jrlaff@users.noreply.github.com>
2025-09-15 22:54:17 +0200

    Add files via upload

---
+++ LayoutCycleExceptionRepro1/LayoutCycleExceptionRepro1/KeysListApplet.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+namespace LayoutCycleExceptionRepro1;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class KeysListApplet : ContentView
+{
+    public ObservableCollection<string> KeyGroups
+    {
+        get
+        {
+            return [
+                "Group 1",
+                "Group 2",
+                "Group 3",
+                "Group 4",
+                "Group 5",
+                "Group 6",
+            ];
+        }
+        set
+        { }
+    }
+
+    public KeysListApplet()
+    {
+        InitializeComponent();
+    }
+}
---
+++ LayoutCycleExceptionRepro1/LayoutCycleExceptionRepro1/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace LayoutCycleExceptionRepro1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Pressed(object sender, EventArgs e)
+    {
+        ScreensContainer.Children.Clear();
+
+        ScreensContainer.Children.Add(new ProfileScreensControl(int.Parse(ScreensCount.Text), ScreensVisible.IsChecked));
+    }
+}
