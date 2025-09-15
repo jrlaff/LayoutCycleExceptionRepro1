8d3eb80330e84e945de664d6069bc16df683c1bf
jrlaff <72731879+jrlaff@users.noreply.github.com>
2025-09-15 22:58:11 +0200

    Add files via upload

---
+++ LayoutCycleExceptionRepro1/ProfileScreensControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace LayoutCycleExceptionRepro1;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class ProfileScreensControl : Grid
+{
+    public ProfileScreensControl(int screensCount, bool allVisible)
+    {
+        InitializeComponent();
+
+        for (int i = 0; i < screensCount; i++)
+            Children.Add(new AppletScreen { IsVisible = allVisible || i == 0 });
+    }
+}
